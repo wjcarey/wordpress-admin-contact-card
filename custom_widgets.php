7815ef5c661e149992e667c1739129f4a32b4c08
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-09-30 11:30:31 -0600

    updated contact info with links

--- custom_widgets.php
+++ custom_widgets.php
@@ -1,13 +1,30 @@
 <?php
 $custom_dashboard_widgets = array(
     'admin-contact-card' => array(
-        'title' => 'Issues and Support',
+        'title' => 'Support And Training',
         'callback' => 'dashboardWidgetContent'
     )
 );
 
 function dashboardWidgetContent() {
     $user = wp_get_current_user();
-    echo "<p>Hello <strong>" . $user->user_login . "</strong>, for assistance, support, and training. Please call me at <strong>Phone Number</strong> or email to <strong>Contact@</strong>.</p>";
-    echo "<p>Admin Name</p>";
+    echo '
+		<p>Hello <strong>' . $user->user_login . '</strong>, for assistance, support, and training. Please contact with your questions via phone or email.</p>
+		<p>
+            <strong>Admin Name</strong>
+            <br>
+            Admin Title
+            <br>
+            <strong>
+                <a href="mailto:contact@emailaddress">contact@emailaddress</a>
+            </strong>
+            <br>
+            <strong>
+                <a href="tel:number">phone number</a>
+            </strong>
+            <br>
+            Denver, CO
+            <br>
+		</p>
+	';
 }
